30b8796808238d9f6d098a7141ba21dbcb6182cb
ArchDev <matthewjobava@gmail.com>
2025-04-20 23:19:14 +0400

    start project

---
+++ src/main/java/me/archdev/staffrelay/StaffRelay.java
@@ -0,0 +1,17 @@
+package me.archdev.staffrelay;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffRelay extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
