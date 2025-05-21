05d8ed3dc003cc7cfc76b0a0997c7ab582fb6e00
ArchDev <matthewjobava@gmail.com>
2025-05-21 16:13:30 +0400

    added: simple discord bot support and config credentials

--- src/main/java/me/archdev/staffrelay/StaffRelay.java
+++ src/main/java/me/archdev/staffrelay/StaffRelay.java
@@ -1,8 +1,14 @@
 package me.archdev.staffrelay;
 
 import lombok.Getter;
+import me.archdev.staffrelay.config.ConfigValues;
 import me.archdev.staffrelay.manager.CommandManager;
 import me.archdev.staffrelay.util.ColorUtil;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,6 +16,9 @@ public final class StaffRelay extends JavaPlugin {
 
     @Getter
     private static StaffRelay instance;
+
+    @Getter
+    private static JDA jda;
 
     @Override
     public void onLoad() {
@@ -20,6 +29,9 @@ public final class StaffRelay extends JavaPlugin {
     public void onEnable() {
         final long startTime = System.currentTimeMillis();
 
+        ConfigValues.loadConfig(getConfig(), instance);
+        createJdaBot();
+
         getCommand("staffrelay").setExecutor(new CommandManager());
 
         final long endTime = System.currentTimeMillis() - startTime;
@@ -29,5 +41,16 @@ public final class StaffRelay extends JavaPlugin {
     @Override
     public void onDisable() {
         instance = null;
+    }
+
+    private void createJdaBot() {
+        if (ConfigValues.botToken == null) return;
+
+        jda = JDABuilder.createDefault(ConfigValues.botToken)
+                .enableIntents(GatewayIntent.GUILD_MESSAGES)
+                .addEventListeners()
+                .setActivity(Activity.of(Activity.ActivityType.valueOf(ConfigValues.botActivity), ConfigValues.botActivityText))
+                .setStatus(OnlineStatus.valueOf(ConfigValues.botStatus))
+                .build();
     }
 }
